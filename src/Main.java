b1d3e1a2be2f27fd6b78cbc8d7c2f0675a11d2fb
HUDAAYOUB <huda3ya@gmail.com>
2023-09-14 18:22:51 +0300

    Q2B

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+class Main {
+    public static void main(String[] args) {
+
+
+        int number = 4;
+
+
+        switch (number) {
+            case 1:
+                System.out.println("ONE");
+                break;
+            case 2:
+                System.out.println("TWO");
+                break;
+            case 3:
+                System.out.println("THREE");
+                break;
+            case 4:
+                System.out.println("FOUR");
+                break;
+            case 5:
+                System.out.println("FIVE");
+                break;
+            case 6:
+                System.out.println("SIX");
+                break;
+        }
+    }
+}
+
+
